64c3d4a8fa99b87378a44a2119124d9796bcd271
Nikita Ignatiev <nignatev@dev.vtb.ru>
2024-04-11 02:46:14 +0300

    Task1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.Task1.Exception.IllegalAnnotationException;
+import org.example.Task1.Exception.OverHeadCount;
+import org.example.Task1.TestClass;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.example.Task1.TestRunner.runTests;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws OverHeadCount, IllegalAnnotationException, InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
+        runTests(TestClass.class);
     }
 }
